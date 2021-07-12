a690a5081a90095e1dda948461fb7bbce0850370
ivis-core <admin@example.com>
2021-07-12 13:06:10 +0200

    Building

--- job.py
+++ job.py
@@ -2,6 +2,5 @@ from ivis import ivis
 import requests
 
 print(ivis.accessToken)
-url = f"http://localhost:8444/{ivis.accessToken}/rest/files/job/file/56/"
 test_file = open("job.py", "rb")
 ivis.upload_file(test_file)
